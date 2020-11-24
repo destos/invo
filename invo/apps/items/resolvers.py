ef7ff68cdc76f91f3304d484bbe9f8b04f271e3d
Patrick Forringer <patrick@forringer.com>
2020-11-23 21:58:26 -0600

    add spaceParents field to items

--- invo/apps/items/resolvers.py
+++ invo/apps/items/resolvers.py
@@ -3,11 +3,20 @@ from ariadne_extended.cursor_pagination import RelayModelMixin
 from .models import Item
 
 from graph.types import query
+from .types import item_interface
 
 
 class ItemResolver(RelayModelMixin, ModelResolver):
     model = Item
     queryset = Item.objects.all()
+
+
+@item_interface.field("spaceParents")
+def resolve_space_parents(item, info, **kwargs):
+    depth = kwargs.get("depth", 100)
+    if item.space:
+        return item.space.get_ancestors(include_self=True)[:depth]
+    return []
 
 
 query.set_field("item", ItemResolver.as_resolver(method="retrieve"))
