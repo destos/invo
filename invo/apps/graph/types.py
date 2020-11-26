c7b398ec30e629dc67bbfcf535ac0753622ee193
Patrick Forringer <patrick@forringer.com>
2020-11-25 22:27:39 -0600

    Adding user and permission/group graphql types

--- invo/apps/graph/types.py
+++ invo/apps/graph/types.py
@@ -1,5 +1,10 @@
-from ariadne import MutationType, QueryType
+from ariadne import MutationType, QueryType, ObjectType
 
 query = QueryType()
 
 mutation = MutationType()
+
+# Built in django type mapping
+user = ObjectType("User")
+group = ObjectType("Group")
+permission = ObjectType("Permission")
