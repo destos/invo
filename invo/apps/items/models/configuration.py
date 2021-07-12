9606bb15a26d9f52b1bcb257564fe3bc19bb8fc7
Patrick Forringer <patrick@forringer.com>
2021-07-11 20:07:07 -0500

    start of variant configuration models
    Not final

---
+++ invo/apps/items/models/configuration.py
@@ -0,0 +1,120 @@
+import pghistory
+import pgtrigger
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.fields import AutoSlugField
+from django_extensions.db.models import TimeStampedModel
+from marshmallow import Schema, fields
+from owners.models import SharedSite
+from safedelete.models import SOFT_DELETE_CASCADE, SafeDeleteModel
+
+from invo.utils.pghistory import enable_history
+
+
+@pgtrigger.register(pgtrigger.Protect(name="protect_deletes", operation=(pgtrigger.Delete)))
+@enable_history(related_name="declaration_history")
+class Declaration(SharedSite, TimeStampedModel, SafeDeleteModel):
+    """
+    A declaration defines what abilities or configuration an item may have, it should allow you to search/filter
+    items by those configurations.
+
+    It does not hold information about a specific configuration.
+
+    Does hold information about compatibilities between different configurations.
+    compatibilities?
+    could there be declarations that share the same variant option and be deemed compatible?
+    Example would be screws.
+
+    Item declarations should be serializeable
+    """
+
+    _safedelete_policy = SOFT_DELETE_CASCADE
+
+    name = models.CharField(_("Name"), max_length=50)
+    # measurements
+    # Differing measurements that use the same name?
+
+    # variants = models.ManyToManyField("Variant")
+    # variant restrictions/validation/generators
+    # Idea create specific variant combinations that are used for relationships, however validation
+    # should be taken into consideration when building them.
+
+    # class Meta:
+
+    template = models.TextField(_("Template"))
+    # Use different json fields in the descriptions
+    def __str__(self):
+        return self.nam
+
+    def add_variant(self, label, name, *variants, conditions=None):
+        return Variant.objects.get_or_create(
+            name=name, declaration=self, defaults=dict(label=label, options=variants)
+        )
+
+    @property
+    def tags(self):
+        return self.variants.values_list("name", flat=True)
+
+    field_mapping = {}
+
+    @cached_property
+    def schema(self):
+        defs = dict()
+        for v in self.variants.all():
+            defs.update({v.name: fields.Str()})
+        self.process_schema_defs(defs)
+        return Schema.from_dict(defs)
+
+    def process_schema_defs(self, defs):
+        pass
+
+
+# also a shared variant?
+class Variant(TimeStampedModel, SafeDeleteModel):
+    label = models.CharField(_("Label"), max_length=50)
+    name = AutoSlugField(
+        _("Name"), populate_from="label", max_length=50, overwrite=False, overwrite_on_add=False
+    )
+    declaration = models.ForeignKey(
+        "Declaration", related_name="variants", on_delete=models.CASCADE
+    )
+    # Facetted?
+    options = ArrayField(models.CharField(max_length=50), blank=True, null=False)
+
+    # assign variant declaration to an item
+    items = models.ManyToManyField("items.Item", related_name="variants", through="VariantThrough")
+
+    class Meta:
+        unique_together = (("name", "declaration"),)
+
+
+# class Conditions(TimeStampedModel, SafeDeleteModel):
+#     "Validators for different combinations of variants"
+
+
+# VariantOption?
+class VariantThrough(models.Model):
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    item = models.ForeignKey("items.Item", on_delete=models.CASCADE)
+    declaration = models.ForeignKey(Declaration, on_delete=models.CASCADE)
+    option = models.CharField(max_length=50)
+
+
+class Configuration(models.Model):
+    # Takes a relationship between all variant options and
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        pass
+
+
+# Need some item taxonomy design.
+# How do we want to handle multiple instances or groups of items that are the same thing, but in different locations
+# Maybe a silent item where you add an item and if the same name is present, you're given the option to
+# Add "another <name of item>" And a new instance is saved that is of that type. Every item has it's own type. Types can be merged?
+# When search
+
+__all__ = ["Declaration", "Variant", "Configuration"]
--- invo/apps/items/tests/test_models.py
+++ invo/apps/items/tests/test_models.py
@@ -1,7 +1,10 @@
+from unittest.mock import patch
+from marshmallow import fields
 from django.test import TestCase
 from items import models
 from model_bakery import baker
 from spaces.models import SpaceNode
+from items.models import Declaration
 
 
 class TestItemQueryset(TestCase):
@@ -129,3 +132,122 @@ class TestTrackedConsumable(TestCase):
         self.assertEqual(str(self.item), "Test Tracked Consumable")
         self.assertEqual(self.item.urn_etype, "items.trackedconsumable")
         self.assertEqual(self.item.irn, "irn:test:items.trackedconsumable:84")
+
+
+class TestItemVariant(TestCase):
+    def setUp(self):
+        self.variant = baker.make("Variant", label="A bit", name="bit", options=[])
+
+    def xtest_attrs(self):
+        self.assertEqual(self.variant.name, "bit")
+        self.assertEqual(self.variant.label, "A bit")
+
+
+class TestItemDeclaration(TestCase):
+    """
+    A screw
+
+    metric screw
+    standards
+    pitch 0.2 to 6
+    calculated variation as a combination of multiple variations?
+
+    Should there be a form of layering where you can apply the declaration and then make variant modifications?
+    say we
+
+    When adding a new declaration variant, you can add more variants and they are added to to the variants as potentially outliers?
+    """
+
+    maxDiff = None
+
+    def setUp(self):
+        self.declaration = baker.make(Declaration, name="Metric screws")
+        self.declaration.add_variant("Diameter", "dia", "1", "2", "3", "4", "5")
+        self.declaration.add_variant("Length", "length", "1", "2", "3")
+        self.declaration.add_variant(
+            "Pitch",
+            "pitch",
+            "0.2",
+            "0.25",
+            "0.3",
+            "0.35",
+            "0.4",
+            "0.5",
+            "1",
+            # TODO: conditions are applied to a variant schema def to validate it in comparison to other
+            # variants or data
+            conditions=dict(),
+        )
+        # self.m3 = baker.make("Configuration", dia=3, pitch=0.5)
+        # self.declaration.add_variant()
+
+    # TODO: create configuration from options
+
+    def xtest_attrs(self):
+        self.assertEqual(self.declaration.name, "Metric screws")
+
+    @patch("items.models.Schema")
+    def xtest_schema_generation(self, mock_Schema):
+        # Generate schema
+        result = self.declaration.schema
+
+        # self.assertEqual(result, mock_Schema.from_dict)
+
+        mock_Schema.from_dict.assert_called()
+        from_dict_arg = mock_Schema.from_dict.call_args.args[0]
+        self.assertDictEqual(
+            from_dict_arg, dict(dia=fields.Str(), length=fields.Str(), pitch=fields.Str())
+        )
+        # _with(
+        #     dict(dia=fields.Str(), length=fields.Str(), pitch=fields.Str())
+        # )
+
+    # def template
+    def xtest_get_variant_tags(self):
+        self.assertSequenceEqual(self.declaration.tags, ["dia", "length", "pitch"])
+
+    def xtest_item_variants(self):
+        """
+        When a item has associated variants you can specify the options used or rely on a specific configuration
+        to provide all information about a item. Then it can also specify a variant option or value to get the final config.
+        """
+
+        item = baker.make(
+            "Item",
+            name="Closet screws",
+            declaration=self.declaration,
+            configuration=self.m3,
+            data=dict(
+                values=dict(
+                    length=52,
+                )
+            ),
+        )
+
+        # Description from declaration template
+        self.assertEqual(item.description, "M3-0.5 x 52")
+
+    def test_declaration_variations(self):
+        pass
+        # dia = range()
+        # m3x10 = dict(
+        #     dia=3,
+        #     length=10,
+        #     pitch=0.2,
+        # )
+        # m8x10 = dict(
+        #     dia=8,
+        #     length=10,
+        #     pitch=1,
+        # )
+
+    # Named variants, example, some standard and then a description
+
+    # Variation generator
+    # MOdifications to the variations, if allowed
+    # 5.1mm length as an example with increments of 1
+    # variations
+
+
+# class TestVariation(TestCase):
+#     pass
